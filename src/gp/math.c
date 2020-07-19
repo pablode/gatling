562883d6c4450c9c9035b6d84928544f21d3e7e3
Pablo Delgado Krämer <private@pablode.com>
2020-07-19 15:27:06 +0200

    gp: update math lib, fix lerp derp.

--- src/gp/math.c
+++ src/gp/math.c
@@ -3,6 +3,13 @@
 #include <math.h>
 #include <assert.h>
 
+void gp_vec3_assign(const gp_vec3 a, gp_vec3 b)
+{
+  b[0] = a[0];
+  b[1] = a[1];
+  b[2] = a[2];
+}
+
 void gp_vec3_add(const gp_vec3 a, const gp_vec3 b, gp_vec3 c)
 {
   c[0] = a[0] + b[0];
@@ -17,7 +24,14 @@ void gp_vec3_sub(const gp_vec3 a, const gp_vec3 b, gp_vec3 c)
   c[2] = a[2] - b[2];
 }
 
-void gp_vec3_div(const gp_vec3 a, float s, gp_vec3 b)
+void gp_vec3_div(const gp_vec3 a, const gp_vec3 b, gp_vec3 c)
+{
+  c[0] = a[0] / b[0];
+  c[1] = a[1] / b[1];
+  c[2] = a[2] / b[2];
+}
+
+void gp_vec3_divs(const gp_vec3 a, float s, gp_vec3 b)
 {
   assert(s != 0.0f);
   b[0] = a[0] / s;
@@ -25,7 +39,24 @@ void gp_vec3_div(const gp_vec3 a, float s, gp_vec3 b)
   b[2] = a[2] / s;
 }
 
-void gp_vec3_mul(const gp_vec3 a, float s, gp_vec3 b)
+void gp_vec3_sdiv(float s, const gp_vec3 a, gp_vec3 b)
+{
+  assert(a[0] != 0.0f);
+  assert(a[1] != 0.0f);
+  assert(a[2] != 0.0f);
+  b[0] = s / a[0];
+  b[1] = s / a[1];
+  b[2] = s / a[2];
+}
+
+void gp_vec3_mul(const gp_vec3 a, const gp_vec3 b, gp_vec3 c)
+{
+  c[0] = a[0] * b[0];
+  c[1] = a[1] * b[1];
+  c[2] = a[2] * b[2];
+}
+
+void gp_vec3_muls(const gp_vec3 a, float s, gp_vec3 b)
 {
   b[0] = a[0] * s;
   b[1] = a[1] * s;
@@ -46,14 +77,28 @@ void gp_vec3_cross(const gp_vec3 a, const gp_vec3 b, gp_vec3 c)
 
 void gp_vec3_lerp(const gp_vec3 a, const gp_vec3 b, float t, gp_vec3 v)
 {
-  v[0] = a[0] + t * (b[0] * a[0]);
-  v[1] = a[1] + t * (b[1] * a[1]);
-  v[2] = a[2] + t * (b[2] * a[2]);
+  v[0] = (1.0f - t) * a[0] + t * b[0];
+  v[1] = (1.0f - t) * a[1] + t * b[1];
+  v[2] = (1.0f - t) * a[2] + t * b[2];
 }
 
 float gp_vec3_length(const gp_vec3 v)
 {
   return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+}
+
+void gp_vec3_max(const gp_vec3 a, const gp_vec3 b, gp_vec3 c)
+{
+  c[0] = fmaxf(a[0], b[0]);
+  c[1] = fmaxf(a[1], b[1]);
+  c[2] = fmaxf(a[2], b[2]);
+}
+
+void gp_vec3_min(const gp_vec3 a, const gp_vec3 b, gp_vec3 c)
+{
+  c[0] = fminf(a[0], b[0]);
+  c[1] = fminf(a[1], b[1]);
+  c[2] = fminf(a[2], b[2]);
 }
 
 float gp_vec3_comp_min(const gp_vec3 v)
@@ -107,6 +152,16 @@ void gp_aabb_merge(const gp_aabb* aabb_a, const gp_aabb* aabb_b, gp_aabb* aabb_c
   aabb_c->max[0] = fmax(aabb_a->max[0], aabb_b->max[0]);
   aabb_c->max[1] = fmax(aabb_a->max[1], aabb_b->max[1]);
   aabb_c->max[2] = fmax(aabb_a->max[2], aabb_b->max[2]);
+}
+
+void gp_aabb_include(const gp_aabb* aabb_a, const gp_vec3 v, gp_aabb* aabb_b)
+{
+  aabb_b->min[0] = fmin(aabb_a->min[0], v[0]);
+  aabb_b->min[1] = fmin(aabb_a->min[1], v[1]);
+  aabb_b->min[2] = fmin(aabb_a->min[2], v[2]);
+  aabb_b->max[0] = fmax(aabb_a->max[0], v[0]);
+  aabb_b->max[1] = fmax(aabb_a->max[1], v[1]);
+  aabb_b->max[2] = fmax(aabb_a->max[2], v[2]);
 }
 
 void gp_aabb_intersect(
--- src/gp/math.h
+++ src/gp/math.h
@@ -7,25 +7,25 @@
 
 typedef float gp_vec3[3];
 
-typedef float gp_vec2[2];
-
 typedef struct gp_aabb {
   gp_vec3 min;
   gp_vec3 max;
 } gp_aabb;
 
-typedef struct gp_triangle {
-  gp_vec3 v[3];
-} gp_triangle;
-
+void gp_vec3_assign(const gp_vec3 a, gp_vec3 b);
 void gp_vec3_add(const gp_vec3 a, const gp_vec3 b, gp_vec3 c);
 void gp_vec3_sub(const gp_vec3 a, const gp_vec3 b, gp_vec3 c);
-void gp_vec3_div(const gp_vec3 a, float s, gp_vec3 b);
-void gp_vec3_mul(const gp_vec3 a, float s, gp_vec3 b);
+void gp_vec3_div(const gp_vec3 a, const gp_vec3 b, gp_vec3 c);
+void gp_vec3_divs(const gp_vec3 a, float s, gp_vec3 b);
+void gp_vec3_sdiv(float s, const gp_vec3 a, gp_vec3 b);
+void gp_vec3_mul(const gp_vec3 a, const gp_vec3 b, gp_vec3 c);
+void gp_vec3_muls(const gp_vec3 a, float s, gp_vec3 b);
 float gp_vec3_dot(const gp_vec3 a, const gp_vec3 b);
 void gp_vec3_cross(const gp_vec3 a, const gp_vec3 b, gp_vec3 c);
 void gp_vec3_lerp(const gp_vec3 a, const gp_vec3 b, float t, gp_vec3 v);
 float gp_vec3_length(const gp_vec3 v);
+void gp_vec3_max(const gp_vec3 a, const gp_vec3 b, gp_vec3 c);
+void gp_vec3_min(const gp_vec3 a, const gp_vec3 b, gp_vec3 c);
 float gp_vec3_comp_min(const gp_vec3 v);
 float gp_vec3_comp_max(const gp_vec3 v);
 
@@ -33,6 +33,7 @@ void gp_aabb_make_smallest(gp_aabb* aabb);
 void gp_aabb_make_biggest(gp_aabb* aabb);
 void gp_aabb_make_from_triangle(const gp_vec3 v_a, const gp_vec3 v_b, const gp_vec3 v_c, gp_aabb* aabb);
 void gp_aabb_merge(const gp_aabb* aabb_a, const gp_aabb* aabb_b, gp_aabb* aabb_c);
+void gp_aabb_include(const gp_aabb* aabb_a, const gp_vec3 v, gp_aabb* aabb_b);
 void gp_aabb_intersect(const gp_aabb* aabb_a, const gp_aabb* aabb_b, gp_aabb* aabb_c);
 void gp_aabb_size(const gp_aabb* aabb, gp_vec3 size);
 float gp_aabb_half_area(const gp_aabb* aabb);
