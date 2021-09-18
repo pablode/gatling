1a1af5ca632e99b42ebd4033f96005cdabffdf40
Pablo Delgado Krämer <private@pablode.com>
2021-09-18 09:09:24 +0200

    cmake: require CMake 3.21 and use CMake-provided glslangValidator path.

--- cmake/ShaderLibrary.cmake
+++ cmake/ShaderLibrary.cmake
@@ -1,15 +1,5 @@
 include(CMakeParseArguments)
 
-if(NOT GATLING_GLSLANG_VALIDATOR)
-  find_program(GATLING_GLSLANG_VALIDATOR glslangValidator)
-endif()
-
-if(NOT GATLING_GLSLANG_VALIDATOR)
-  message(FATAL_ERROR "glslangValidator not found!")
-endif()
-
-message(STATUS "Found glslangValidator: ${GATLING_GLSLANG_VALIDATOR}")
-
 function(add_shader_library target)
 
   # Read shader files and includes
@@ -36,7 +26,7 @@ function(add_shader_library target)
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
       COMMENT "Compiling GLSL shader ${shader_name}"
       COMMAND ${CMAKE_COMMAND} -E make_directory ${header_dir}
-      COMMAND ${GATLING_GLSLANG_VALIDATOR} -D -e CSMain --target-env vulkan1.1 --vn "SPV_${shader_name_we}_${shader_ext}" -o ${header_path} --quiet ${shader_path}
+      COMMAND ${Vulkan_GLSLANG_VALIDATOR_EXECUTABLE} -D -e CSMain --target-env vulkan1.1 --vn "SPV_${shader_name_we}_${shader_ext}" -o ${header_path} --quiet ${shader_path}
       DEPENDS ${TARGET_INCLUDES}
     )
 
