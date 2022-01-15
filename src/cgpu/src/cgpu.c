895d1d82fc9791d9080ba700fa36c76168eda4d9
Pablo Delgado Krämer <private@pablode.com>
2022-01-15 23:23:12 +0100

    cmake/cgpu: add comments, remove obsolete code.

--- src/cgpu/src/cgpu.c
+++ src/cgpu/src/cgpu.c
@@ -2162,10 +2162,6 @@ CGPU_API CgpuResult CGPU_CDECL cgpu_cmd_copy_buffer_to_image(
   if (!cgpu_resolve_command_buffer(command_buffer, &icommand_buffer)) {
     return CGPU_FAIL_INVALID_HANDLE;
   }
-  cgpu_idevice* idevice;
-  if (!cgpu_resolve_device(icommand_buffer->device, &idevice)) {
-    return CGPU_FAIL_INVALID_HANDLE;
-  }
   cgpu_ibuffer* ibuffer;
   if (!cgpu_resolve_buffer(buffer, &ibuffer)) {
     return CGPU_FAIL_INVALID_HANDLE;
