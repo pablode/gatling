faac488e5276b462ec8a9b94458169bb6153b18f
Pablo Delgado Krämer <private@pablode.com>
2022-05-26 11:40:58 +0200

    shadergen: inline shade_hit() function

--- src/shadergen/shaders/main.comp.hlsl
+++ src/shadergen/shaders/main.comp.hlsl
@@ -34,95 +34,6 @@ struct Sample_state
     float3 radiance;
 };
 
-bool shade_hit(inout Sample_state state, in Hit_info hit)
-{
-    face f = faces[hit.face_idx];
-    fvertex v_0 = vertices[f.v_0];
-    fvertex v_1 = vertices[f.v_1];
-    fvertex v_2 = vertices[f.v_2];
-
-    /* Geometric normal. */
-    float3 p_0 = v_0.field1.xyz;
-    float3 p_1 = v_1.field1.xyz;
-    float3 p_2 = v_2.field1.xyz;
-    float3 geom_normal = normalize(cross(p_1 - p_0, p_2 - p_0));
-
-    /* Shading normal. */
-    float3 n_0 = v_0.field2.xyz;
-    float3 n_1 = v_1.field2.xyz;
-    float3 n_2 = v_2.field2.xyz;
-
-    float2 bc = hit.bc;
-    float3 normal = normalize(
-        n_0 * (1.0 - bc.x - bc.y) +
-        n_1 * bc.x +
-        n_2 * bc.y
-    );
-
-#if AOV_ID == AOV_ID_NORMAL
-    state.radiance = (normal + float3(1.0, 1.0, 1.0)) * 0.5;
-    return false;
-#endif
-
-    /* Tangent and bitangent. */
-    float3 L = abs(normal.z) < 0.999 ? float3(0.0, 0.0, 1.0) : float3(1.0, 0.0, 0.0);
-    float3 tangent = normalize(cross(L, normal));
-    float3 bitangent = cross(normal, tangent);
-
-    /* Flip normals to side of incident ray. */
-    if (dot(geom_normal, state.ray_dir) > 0.0)
-    {
-        geom_normal *= -1.0f;
-    }
-    if (dot(normal, state.ray_dir) > 0.0)
-    {
-        normal *= -1.0f;
-    }
-
-    /* EDF evaluation. */
-    Shading_state_material shading_state_material;
-    shading_state_material.normal = normal;
-    shading_state_material.geom_normal = geom_normal;
-    shading_state_material.position = hit.pos;
-    shading_state_material.tangent_u[0] = tangent;
-    shading_state_material.tangent_v[0] = bitangent;
-    shading_state_material.animation_time = 0.0;
-
-    Edf_evaluate_data edf_evaluate_data;
-    edf_evaluate_data.k1 = -state.ray_dir;
-    mdl_edf_emission_init(f.mat_idx, shading_state_material);
-    mdl_edf_emission_evaluate(f.mat_idx, edf_evaluate_data, shading_state_material);
-
-    float3 emission_intensity = mdl_edf_emission_intensity(f.mat_idx, shading_state_material);
-
-    /* BSDF (importance) sampling. */
-    Bsdf_sample_data bsdf_sample_data;
-    bsdf_sample_data.ior1 = BSDF_USE_MATERIAL_IOR;
-    bsdf_sample_data.ior2 = BSDF_USE_MATERIAL_IOR;
-    bsdf_sample_data.k1 = -state.ray_dir;
-    bsdf_sample_data.xi = pcg4d_next(state.rng_state);
-    mdl_bsdf_scattering_init(f.mat_idx, shading_state_material);
-    mdl_bsdf_scattering_sample(f.mat_idx, bsdf_sample_data, shading_state_material);
-
-    /* Handle results. */
-    state.radiance += state.throughput * edf_evaluate_data.edf * emission_intensity;
-
-    if ((bsdf_sample_data.event_type & BSDF_EVENT_ABSORB) != 0)
-    {
-        return false;
-    }
-
-    state.throughput *= bsdf_sample_data.bsdf_over_pdf;
-
-    /* Prepare next ray. */
-    bool is_transmission = ((bsdf_sample_data.event_type & BSDF_EVENT_TRANSMISSION) != 0);
-
-    state.ray_dir = bsdf_sample_data.k2;
-    state.ray_origin = offset_ray_origin(hit.pos, geom_normal * (is_transmission ? -1.0 : 1.0));
-
-    return true;
-}
-
 bool russian_roulette(in float random_float, inout float3 throughput)
 {
     float max_throughput = max(throughput.r, max(throughput.g, throughput.b));
@@ -157,15 +68,15 @@ float3 evaluate_sample(inout uint4 rng_state,
         ray.tmax   = FLOAT_MAX;
         ray.dir    = state.ray_dir;
 
-        Hit_info hit_info;
+        Hit_info hit;
 
-        bool found_hit = find_hit_closest(ray, hit_info);
+        bool found_hit = find_hit_closest(ray, hit);
 
 #if AOV_ID == AOV_ID_DEBUG_BVH_STEPS
-        state.radiance = float3(float(hit_info.bvh_steps), 0.0, 0.0);
+        state.radiance = float3(float(hit.bvh_steps), 0.0, 0.0);
         break;
 #elif AOV_ID == AOV_ID_DEBUG_TRI_TESTS
-        state.radiance = float3(0.0, 0.0, float(hit_info.tri_tests));
+        state.radiance = float3(0.0, 0.0, float(hit.tri_tests));
         break;
 #endif
 
@@ -202,7 +113,7 @@ float3 evaluate_sample(inout uint4 rng_state,
 
             /* Don't continue if we are on the same plane as the triangle. Seen from the side,
              * it would be infinitely thin - and would therefore have no emissive area. */
-            float3 to_light = P - hit_info.pos;
+            float3 to_light = P - hit.pos;
 
             float3 geom_normal = normalize(cross(p1 - p0, p2 - p0));
 
@@ -213,7 +124,7 @@ float3 evaluate_sample(inout uint4 rng_state,
             if (light_dist > 0.0 && !is_orthogonal)
             {
                 to_light /= light_dist;
-                float3 hit_offset = offset_ray_origin(hit_info.pos, to_light);
+                float3 hit_offset = offset_ray_origin(hit.pos, to_light);
                 float3 light_offset = offset_ray_origin(P, -to_light);
 
                 ray.origin = hit_offset;
@@ -233,11 +144,90 @@ float3 evaluate_sample(inout uint4 rng_state,
         break;
 #endif
 
-        bool continue_sampling = shade_hit(state, hit_info);
-
-        if (!continue_sampling)
         {
+            face f = faces[hit.face_idx];
+            fvertex v_0 = vertices[f.v_0];
+            fvertex v_1 = vertices[f.v_1];
+            fvertex v_2 = vertices[f.v_2];
+
+            /* Geometric normal. */
+            float3 p_0 = v_0.field1.xyz;
+            float3 p_1 = v_1.field1.xyz;
+            float3 p_2 = v_2.field1.xyz;
+            float3 geom_normal = normalize(cross(p_1 - p_0, p_2 - p_0));
+
+            /* Shading normal. */
+            float3 n_0 = v_0.field2.xyz;
+            float3 n_1 = v_1.field2.xyz;
+            float3 n_2 = v_2.field2.xyz;
+
+            float2 bc = hit.bc;
+            float3 normal = normalize(
+                n_0 * (1.0 - bc.x - bc.y) +
+                n_1 * bc.x +
+                n_2 * bc.y
+            );
+
+        #if AOV_ID == AOV_ID_NORMAL
+            state.radiance = (normal + float3(1.0, 1.0, 1.0)) * 0.5;
             break;
+        #endif
+
+            /* Tangent and bitangent. */
+            float3 L = abs(normal.z) < 0.999 ? float3(0.0, 0.0, 1.0) : float3(1.0, 0.0, 0.0);
+            float3 tangent = normalize(cross(L, normal));
+            float3 bitangent = cross(normal, tangent);
+
+            /* Flip normals to side of incident ray. */
+            if (dot(geom_normal, state.ray_dir) > 0.0)
+            {
+                geom_normal *= -1.0f;
+            }
+            if (dot(normal, state.ray_dir) > 0.0)
+            {
+                normal *= -1.0f;
+            }
+
+            /* EDF evaluation. */
+            Shading_state_material shading_state_material;
+            shading_state_material.normal = normal;
+            shading_state_material.geom_normal = geom_normal;
+            shading_state_material.position = hit.pos;
+            shading_state_material.tangent_u[0] = tangent;
+            shading_state_material.tangent_v[0] = bitangent;
+            shading_state_material.animation_time = 0.0;
+
+            Edf_evaluate_data edf_evaluate_data;
+            edf_evaluate_data.k1 = -state.ray_dir;
+            mdl_edf_emission_init(f.mat_idx, shading_state_material);
+            mdl_edf_emission_evaluate(f.mat_idx, edf_evaluate_data, shading_state_material);
+
+            float3 emission_intensity = mdl_edf_emission_intensity(f.mat_idx, shading_state_material);
+
+            /* BSDF (importance) sampling. */
+            Bsdf_sample_data bsdf_sample_data;
+            bsdf_sample_data.ior1 = BSDF_USE_MATERIAL_IOR;
+            bsdf_sample_data.ior2 = BSDF_USE_MATERIAL_IOR;
+            bsdf_sample_data.k1 = -state.ray_dir;
+            bsdf_sample_data.xi = pcg4d_next(state.rng_state);
+            mdl_bsdf_scattering_init(f.mat_idx, shading_state_material);
+            mdl_bsdf_scattering_sample(f.mat_idx, bsdf_sample_data, shading_state_material);
+
+            /* Handle results. */
+            state.radiance += state.throughput * edf_evaluate_data.edf * emission_intensity;
+
+            if ((bsdf_sample_data.event_type & BSDF_EVENT_ABSORB) != 0)
+            {
+                break;
+            }
+
+            state.throughput *= bsdf_sample_data.bsdf_over_pdf;
+
+            /* Prepare next ray. */
+            bool is_transmission = ((bsdf_sample_data.event_type & BSDF_EVENT_TRANSMISSION) != 0);
+
+            state.ray_dir = bsdf_sample_data.k2;
+            state.ray_origin = offset_ray_origin(hit.pos, geom_normal * (is_transmission ? -1.0 : 1.0));
         }
 
         if (bounce >= PC.RR_BOUNCE_OFFSET)
