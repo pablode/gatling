13d1960c8383486c6d79d5ce7abee447dd0a3046
Pablo Delgado Krämer <private@pablode.com>
2022-06-06 14:57:56 +0200

    imgio: fix missing extern "C" guard

--- src/imgio/include/imgio.h
+++ src/imgio/include/imgio.h
@@ -23,9 +23,17 @@
 #include "error_codes.h"
 #include "img.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int imgio_load_img(const char* file_path,
                    struct imgio_img* img);
 
 void imgio_free_img(struct imgio_img* img);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
