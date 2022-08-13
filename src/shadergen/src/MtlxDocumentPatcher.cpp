5df5e0dc743c303519a91b555aba0abd54b55e81
Pablo Delgado Krämer <private@pablode.com>
2022-08-13 16:09:42 +0200

    shadergen: patch MaterialX document file paths for use in MDL

--- src/shadergen/src/MtlxDocumentPatcher.cpp
+++ src/shadergen/src/MtlxDocumentPatcher.cpp
@@ -17,10 +17,63 @@
 
 #include "MtlxDocumentPatcher.h"
 
+#include <MaterialXCore/Types.h>
+
+#include <assert.h>
+
+namespace mx = MaterialX;
+
+void _SanitizeFilePath(std::string& path)
+{
+  // The MDL SDK does not take raw OS paths. First, only forward-facing slashes are allowed.
+  std::replace(path.begin(), path.end(), '\\', '/');
+
+  // Second, only UNIX-style absolute paths ('/' prefix, no double colon) are valid.
+  bool hasDriveSpecifier = path.size() >= 2 && path[1] == ':';
+
+  if (hasDriveSpecifier)
+  {
+    path[1] = path[0];
+    path[0] = '/';
+  }
+}
+
+void _SanitizeFilePaths(MaterialX::DocumentPtr document)
+{
+  for (auto treeIt = document->traverseTree(); treeIt != mx::TreeIterator::end(); ++treeIt)
+  {
+    mx::ElementPtr elem = treeIt.getElement();
+
+    mx::PortElementPtr portElem = elem->asA<mx::PortElement>();
+    if (!portElem)
+    {
+      continue;
+    }
+
+    std::string portType = portElem->getType();
+    if (portType != mx::FILENAME_TYPE_STRING)
+    {
+      continue;
+    }
+
+    mx::ValuePtr valuePtr = portElem->getValue();
+    if (!valuePtr)
+    {
+      continue;
+    }
+
+    std::string path = valuePtr->asA<std::string>();
+
+    _SanitizeFilePath(path);
+
+    portElem->setValue(path, mx::FILENAME_TYPE_STRING);
+  }
+}
+
 namespace sg
 {
   void MtlxDocumentPatcher::patch(MaterialX::DocumentPtr document)
   {
-    // TODO
+    _SanitizeFilePaths(document);
   }
 }
