2f4e3feea7a4f7b776c17931a59e79b34eb0f4e4
Pablo Delgado Krämer <private@pablode.com>
2022-08-24 20:34:41 +0200

    hdGatling: simplify material network patcher logic

--- src/hdGatling/MaterialNetworkPatcher.cpp
+++ src/hdGatling/MaterialNetworkPatcher.cpp
@@ -38,171 +38,141 @@ TF_DEFINE_PRIVATE_TOKENS(
   (scale)
 );
 
-namespace detail
+void _PatchUsdTypes(HdMaterialNetwork2& network)
 {
-  class PatcherBase
+  for (auto& pathNodePair : network.nodes)
   {
-  public:
-    virtual void PatchNetwork(HdMaterialNetwork2& network)
+    HdMaterialNode2& node = pathNodePair.second;
+
+    auto& parameters = node.parameters;
+
+    for (auto& tokenValuePair : parameters)
     {
-      for (auto& pathNodePair : network.nodes)
+      VtValue& value = tokenValuePair.second;
+
+      // Workaround for HdMtlxConvertToString not handling the TfToken type:
+      // https://github.com/PixarAnimationStudios/USD/blob/3abc46452b1271df7650e9948fef9f0ce602e3b2/pxr/imaging/hdMtlx/hdMtlx.cpp#L117
+      if (value.IsHolding<TfToken>())
       {
-        PatchNode(pathNodePair.second);
+        value = value.Cast<std::string>();
+      }
+
+      // When serializing the network to a MaterialX document again, the SdfAssetPath
+      // gets replaced by its non-resolved path and we don't have any other way of resolving
+      // it at a later point in time, since this is done by the Sdf/Ar layer.
+      if (value.IsHolding<SdfAssetPath>())
+      {
+        value = value.UncheckedGet<SdfAssetPath>().GetResolvedPath();
       }
     }
+  }
+}
 
-    virtual void PatchNode(HdMaterialNode2& node)
-    {
-    }
-  };
-
-  class UsdTypePatcher : public PatcherBase
+// Some of Sketchfab's auto-converted assets encode the roughness on the UsdPreviewSurface
+// node with a 'glossiness' input. See "Screen Space Reflection Demo: Follmann 2.OG" scene:
+// https://sketchfab.com/3d-models/screen-space-reflection-demo-follmann-2og-6164eed28c464c94be8f5268240dc864
+void _PatchUsdPreviewSurfaceGlossiness(HdMaterialNetwork2& network)
+{
+  for (auto& pathNodePair : network.nodes)
   {
-  public:
-    void PatchNode(HdMaterialNode2& node) override
+    HdMaterialNode2& node = pathNodePair.second;
+
+    if (node.nodeTypeId != _tokens->ND_UsdPreviewSurface_surfaceshader)
     {
-      auto& parameters = node.parameters;
-
-      for (auto& tokenValuePair : parameters)
-      {
-        VtValue& value = tokenValuePair.second;
-
-        // Workaround for HdMtlxConvertToString not handling the TfToken type:
-        // https://github.com/PixarAnimationStudios/USD/blob/3abc46452b1271df7650e9948fef9f0ce602e3b2/pxr/imaging/hdMtlx/hdMtlx.cpp#L117
-        if (value.IsHolding<TfToken>())
-        {
-          value = value.Cast<std::string>();
-        }
-
-        // When serializing the network to a MaterialX document again, the SdfAssetPath
-        // gets replaced by its non-resolved path and we don't have any other way of resolving
-        // it at a later point in time, since this is done by the Sdf/Ar layer.
-        if (value.IsHolding<SdfAssetPath>())
-        {
-          value = value.UncheckedGet<SdfAssetPath>().GetResolvedPath();
-        }
-      }
+      return;
     }
-  };
 
-  // Some of Sketchfab's auto-converted assets encode the roughness on the UsdPreviewSurface
-  // node with a 'glossiness' input. See "Screen Space Reflection Demo: Follmann 2.OG" scene:
-  // https://sketchfab.com/3d-models/screen-space-reflection-demo-follmann-2og-6164eed28c464c94be8f5268240dc864
-  class UsdPreviewSurfaceGlossinessPatcher : public PatcherBase
+    auto& parameters = node.parameters;
+
+    auto glossinessParam = parameters.find(_tokens->glossiness);
+    if (glossinessParam == parameters.end())
+    {
+      return;
+    }
+
+    VtValue value = glossinessParam->second;
+    if (!value.IsHolding<float>())
+    {
+      return;
+    }
+
+    TF_WARN("patching UsdPreviewSurface:glossiness input (set %s to disable)",
+      ENVVAR_DISABLE_PATCH_USDPREVIEWSURFACE_GLOSSINESS);
+
+    float glossiness = value.UncheckedGet<float>();
+    float roughness = 1.0f - glossiness;
+
+    parameters[TfToken("roughness")] = roughness;
+    parameters.erase(glossinessParam);
+  }
+}
+
+// J CUBE's Maneki asset tries to read a normal map using a UsdUVTexture node,
+// however the bias and scale parameters which usually convert the [0, 1] image
+// values to [-1, 1] vectors are missing: https://j-cube.jp/solutions/multiverse/assets
+void _PatchUsdPreviewSurfaceNormalInputConnection(HdMaterialNetwork2& network, HdMaterialConnection2& connection)
+{
+  HdMaterialNode2& upstreamNode = network.nodes[connection.upstreamNode];
+
+  if (upstreamNode.nodeTypeId != _tokens->ND_UsdUVTexture)
   {
-  public:
-    void PatchNode(HdMaterialNode2& node) override
-    {
-      if (node.nodeTypeId != _tokens->ND_UsdPreviewSurface_surfaceshader)
-      {
-        return;
-      }
+    return;
+  }
 
-      auto& parameters = node.parameters;
+  auto& upstreamNodeParams = upstreamNode.parameters;
+  bool hasScale = upstreamNodeParams.find(_tokens->scale) != upstreamNodeParams.end();
+  bool hasBias = upstreamNodeParams.find(_tokens->bias) != upstreamNodeParams.end();
 
-      auto glossinessParam = parameters.find(_tokens->glossiness);
-      if (glossinessParam == parameters.end())
-      {
-        return;
-      }
-
-      VtValue value = glossinessParam->second;
-      if (!value.IsHolding<float>())
-      {
-        return;
-      }
-
-      TF_WARN("patching UsdPreviewSurface:glossiness input (set %s to disable)",
-        ENVVAR_DISABLE_PATCH_USDPREVIEWSURFACE_GLOSSINESS);
-
-      float glossiness = value.UncheckedGet<float>();
-      float roughness = 1.0f - glossiness;
-
-      parameters[TfToken("roughness")] = roughness;
-      parameters.erase(glossinessParam);
-    }
-  };
-
-  // J CUBE's Maneki asset tries to read a normal map using a UsdUVTexture node,
-  // however the bias and scale parameters which usually convert the [0, 1] image
-  // values to [-1, 1] vectors are missing: https://j-cube.jp/solutions/multiverse/assets
-  class UsdPreviewSurfaceNormalMapPatcher : public PatcherBase
+  if (hasScale || hasBias)
   {
-  public:
-    void PatchNetwork(HdMaterialNetwork2& network) override
+    return;
+  }
+
+  TF_WARN("patching UsdPreviewSurface:normal to have scaled and biased reader (set %s to disable)",
+    ENVVAR_DISABLE_PATCH_USDPREVIEWSURFACE_NORMALMAP);
+
+  upstreamNodeParams[_tokens->scale] = GfVec4f(2.0f, 2.0f, 2.0f, 1.0f);
+  upstreamNodeParams[_tokens->bias] = GfVec4f(-1.0f, -1.0f, -1.0f, 0.0f);
+}
+
+void _PatchUsdPreviewSurfaceNormalMap(HdMaterialNetwork2& network)
+{
+  for (auto& pathNodePair : network.nodes)
+  {
+    HdMaterialNode2& node = pathNodePair.second;
+    if (node.nodeTypeId != _tokens->ND_UsdPreviewSurface_surfaceshader)
     {
-      for (auto& pathNodePair : network.nodes)
-      {
-        HdMaterialNode2& node = pathNodePair.second;
-        if (node.nodeTypeId != _tokens->ND_UsdPreviewSurface_surfaceshader)
-        {
-          continue;
-        }
-
-        auto& inputs = node.inputConnections;
-
-        auto& normalInputIt = inputs.find(_tokens->normal);
-        if (normalInputIt == inputs.end())
-        {
-          continue;
-        }
-
-        auto& connections = normalInputIt->second;
-        for (HdMaterialConnection2& connection : connections)
-        {
-          PatchNormalInputConnection(network, connection);
-        }
-      }
+      continue;
     }
 
-  private:
-    void PatchNormalInputConnection(HdMaterialNetwork2& network, HdMaterialConnection2& connection)
+    auto& inputs = node.inputConnections;
+
+    auto& normalInputIt = inputs.find(_tokens->normal);
+    if (normalInputIt == inputs.end())
     {
-      HdMaterialNode2& upstreamNode = network.nodes[connection.upstreamNode];
-
-      if (upstreamNode.nodeTypeId != _tokens->ND_UsdUVTexture)
-      {
-        return;
-      }
-
-      auto& upstreamNodeParams = upstreamNode.parameters;
-      bool hasScale = upstreamNodeParams.find(_tokens->scale) != upstreamNodeParams.end();
-      bool hasBias = upstreamNodeParams.find(_tokens->bias) != upstreamNodeParams.end();
-
-      if (hasScale || hasBias)
-      {
-        return;
-      }
-
-      TF_WARN("patching UsdPreviewSurface:normal to have scaled and biased reader (set %s to disable)",
-        ENVVAR_DISABLE_PATCH_USDPREVIEWSURFACE_NORMALMAP);
-
-      upstreamNodeParams[_tokens->scale] = GfVec4f(2.0f, 2.0f, 2.0f, 1.0f);
-      upstreamNodeParams[_tokens->bias] = GfVec4f(-1.0f, -1.0f, -1.0f, 0.0f);
+      continue;
     }
-  };
+
+    auto& connections = normalInputIt->second;
+    for (HdMaterialConnection2& connection : connections)
+    {
+      _PatchUsdPreviewSurfaceNormalInputConnection(network, connection);
+    }
+  }
 }
 
 void MaterialNetworkPatcher::Patch(HdMaterialNetwork2& network)
 {
-  using PatcherBasePtr = std::unique_ptr<detail::PatcherBase>;
-
-  std::vector<PatcherBasePtr> patchers;
-
-  patchers.push_back(std::make_unique<detail::UsdTypePatcher>());
+  _PatchUsdTypes(network);
 
   if (!getenv(ENVVAR_DISABLE_PATCH_USDPREVIEWSURFACE_GLOSSINESS))
   {
-    patchers.push_back(std::make_unique<detail::UsdPreviewSurfaceGlossinessPatcher>());
-  }
-  if (!getenv(ENVVAR_DISABLE_PATCH_USDPREVIEWSURFACE_NORMALMAP))
-  {
-    patchers.push_back(std::make_unique<detail::UsdPreviewSurfaceNormalMapPatcher>());
+    _PatchUsdPreviewSurfaceGlossiness(network);
   }
 
-  for (auto& patcher : patchers)
+  if (!getenv(ENVVAR_DISABLE_PATCH_USDPREVIEWSURFACE_NORMALMAP))
   {
-    patcher->PatchNetwork(network);
+    _PatchUsdPreviewSurfaceNormalMap(network);
   }
 }
 
