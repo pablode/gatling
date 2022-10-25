21e45b05915a52a5354ef31c9c339d5e9ab61398
Pablo Delgado Krämer <private@pablode.com>
2022-10-26 00:39:21 +0200

    mdl-image-plugin: remove plugin
    This hack can be replaced by the resolve_resources execution env option!

--- src/shadergen/src/MdlRuntime.cpp
+++ src/shadergen/src/MdlRuntime.cpp
@@ -52,15 +52,6 @@ namespace sg
     m_logger = mi::base::Handle<MdlLogger>(new MdlLogger());
     m_config->set_logger(m_logger.get());
 
-    // Need to register dummy image loading plugin because `resolve_resources=false` execution context option does not work as expected
-    auto pluginConfigApi = mi::base::Handle<mi::neuraylib::IPlugin_configuration>(m_neuray->get_api_component<mi::neuraylib::IPlugin_configuration>());
-    std::string pluginFilePath = std::string(resourcePath) + std::string("/mdl-image-plugin" MI_BASE_DLL_FILE_EXT);
-    if (pluginConfigApi->load_plugin_library(pluginFilePath.c_str()) != 0)
-    {
-      m_logger->message(mi::base::MESSAGE_SEVERITY_FATAL, "Unable to load image plugin");
-      return false;
-    }
-
     if (m_neuray->start() != 0)
     {
       m_logger->message(mi::base::MESSAGE_SEVERITY_FATAL, "Unable to start Neuray");
