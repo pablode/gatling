6ab9bdb785cce68b0248bc77528c78e2224424e7
Pablo Delgado Krämer <private@pablode.com>
2023-01-23 11:40:15 +0100

    gi: add GlslSourceStitcher

---
+++ src/gi/src/sg/GlslSourceStitcher.cpp
@@ -0,0 +1,117 @@
+//
+// Copyright (C) 2023 Pablo Delgado Krämer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "GlslSourceStitcher.h"
+
+#include <string>
+#include <fstream>
+
+namespace detail
+{
+  bool readTextFromFile(const fs::path& filePath, std::string& text)
+  {
+    std::ifstream file(filePath, std::ios_base::in | std::ios_base::binary);
+    if (!file.is_open())
+    {
+      return false;
+    }
+    file.seekg(0, std::ios_base::end);
+    text.resize(file.tellg(), ' ');
+    file.seekg(0, std::ios_base::beg);
+    file.read(&text[0], text.size());
+    return file.good();
+  }
+}
+
+namespace gi::sg
+{
+  GlslSourceStitcher::GlslSourceStitcher()
+  {
+    // Full float precision so that we don't cut off epsilons
+    m_source.precision(std::numeric_limits<float>::max_digits10);
+    // Ensure no integer literals are emitted for floats
+    m_source.setf(std::ios::fixed | std::ios::showpoint);
+  }
+
+  void GlslSourceStitcher::appendVersion()
+  {
+    m_source << "#version 460 core\n";
+  }
+
+  void GlslSourceStitcher::appendDefine(std::string_view name)
+  {
+    m_source << "#define " << name << "\n";
+  }
+
+  void GlslSourceStitcher::appendDefine(std::string_view name, int32_t value)
+  {
+    m_source << "#define " << name << " " << value << "\n";
+  }
+
+  void GlslSourceStitcher::appendDefine(std::string_view name, uint32_t value)
+  {
+    m_source << "#define " << name << " " << value << "u\n";
+  }
+
+  void GlslSourceStitcher::appendDefine(std::string_view name, float value)
+  {
+    m_source << "#define " << name << " " << value << "\n";
+  }
+
+  void GlslSourceStitcher::appendRequiredExtension(std::string_view name)
+  {
+    m_source << "#extension " << name << ": require\n";
+  }
+
+  void GlslSourceStitcher::appendString(std::string_view value)
+  {
+    m_source << value;
+  }
+
+  bool GlslSourceStitcher::appendSourceFile(fs::path path)
+  {
+    std::string text;
+    if (!detail::readTextFromFile(path, text))
+    {
+      return false;
+    }
+
+    appendString(text);
+    return true;
+  }
+
+  bool GlslSourceStitcher::replaceFirst(std::string_view substring, std::string_view replacement)
+  {
+    std::string tmp = m_source.str();
+
+    size_t location = tmp.find(substring);
+    if (location == std::string::npos)
+    {
+      return false;
+    }
+
+    tmp.replace(location, substring.length(), replacement);
+
+    m_source = std::stringstream(tmp);
+    return true;
+  }
+
+  std::string GlslSourceStitcher::source()
+  {
+    return m_source.str();
+  }
+}
---
+++ src/gi/src/sg/GlslSourceStitcher.h
@@ -0,0 +1,54 @@
+//
+// Copyright (C) 2023 Pablo Delgado Krämer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#pragma once
+
+#include <stdint.h>
+#include <string_view>
+#include <filesystem>
+#include <sstream>
+
+namespace fs = std::filesystem;
+
+namespace gi::sg
+{
+  class GlslSourceStitcher
+  {
+  public:
+    GlslSourceStitcher();
+
+    void appendVersion();
+
+    void appendDefine(std::string_view name);
+    void appendDefine(std::string_view name, int32_t value);
+    void appendDefine(std::string_view name, uint32_t value);
+    void appendDefine(std::string_view name, float value);
+
+    void appendRequiredExtension(std::string_view name);
+
+    void appendString(std::string_view value);
+
+    bool appendSourceFile(fs::path path);
+
+    bool replaceFirst(std::string_view substring, std::string_view replacement);
+
+    std::string source();
+
+  private:
+    std::stringstream m_source;
+  };
+}
