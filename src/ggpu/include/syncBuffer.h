695374c67978a3bf73423b3cd27f7fcaedfe0f25
Pablo Delgado Krämer <private@pablode.com>
2023-05-16 13:29:34 +0200

    ggpu: CPU -> GPU synchronized buffer

---
+++ src/ggpu/include/syncBuffer.h
@@ -0,0 +1,92 @@
+//
+// Copyright (C) 2023 Pablo Delgado Krämer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <cgpu.h>
+
+#include "resizableBuffer.h"
+
+namespace gtl
+{
+  class GgpuStager;
+
+  class GgpuSyncBuffer
+  {
+  public:
+    enum class UpdateStrategy
+    {
+      PersistentMapping,
+      OptimalStaging
+    };
+
+  public:
+    GgpuSyncBuffer(CgpuDevice device,
+                   GgpuStager& stager,
+                   uint64_t elementSize,
+                   UpdateStrategy updateStrategy = UpdateStrategy::OptimalStaging,
+                   CgpuBufferUsageFlags bufferUsage = 0);
+
+    ~GgpuSyncBuffer();
+
+  public:
+    template<typename T>
+    T* getForWriting(uint64_t offset, uint64_t range)
+    {
+      return (T*) getForWriting(offset * m_elementSize, range * m_elementSize);
+    }
+
+    template<typename T>
+    T* getForReading(uint64_t offset, uint64_t range)
+    {
+      return (T*) getForReading(offset * m_elementSize, range * m_elementSize);
+    }
+
+    bool resize(CgpuDevice device,
+                CgpuCommandBuffer commandBuffer,
+                uint64_t newSize);
+
+    CgpuBuffer buffer() const;
+
+    uint64_t byteSize() const;
+
+    bool commitChanges(CgpuCommandBuffer commandBuffer);
+
+  private:
+    uint8_t* getForReading(uint64_t byteOffset, uint64_t byteSize);
+
+    uint8_t* getForWriting(uint64_t byteOffset, uint64_t byteSize);
+
+  private:
+    CgpuDevice m_device;
+    GgpuStager& m_stager;
+    uint64_t m_elementSize;
+    UpdateStrategy m_updateStrategy;
+
+    uint64_t m_size = 0;
+    GgpuResizableBuffer m_deviceBuffer; // only for OptimalStaging strategy
+    GgpuResizableBuffer m_hostBuffer;
+
+    uint8_t* m_mappedHostMem = nullptr;
+    uint64_t m_dirtyRangeBegin = 0;
+    uint64_t m_dirtyRangeEnd = 0;
+  };
+}
---
+++ src/ggpu/src/syncBuffer.cpp
@@ -0,0 +1,145 @@
+//
+// Copyright (C) 2023 Pablo Delgado Krämer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "syncBuffer.h"
+
+#include "stager.h"
+
+#include <algorithm>
+
+namespace gtl
+{
+  GgpuSyncBuffer::GgpuSyncBuffer(CgpuDevice device,
+                                 GgpuStager& stager,
+                                 uint64_t elementSize,
+                                 UpdateStrategy updateStrategy,
+                                 CgpuBufferUsageFlags bufferUsage)
+    : m_device(device)
+    , m_stager(stager)
+    , m_elementSize(elementSize)
+    , m_updateStrategy(updateStrategy)
+    , m_hostBuffer(m_device,
+                   CGPU_BUFFER_USAGE_FLAG_STORAGE_BUFFER | CGPU_BUFFER_USAGE_FLAG_TRANSFER_SRC,
+                   CGPU_MEMORY_PROPERTY_FLAG_HOST_VISIBLE | CGPU_MEMORY_PROPERTY_FLAG_HOST_CACHED |
+                     (updateStrategy == UpdateStrategy::PersistentMapping ? CGPU_MEMORY_PROPERTY_FLAG_DEVICE_LOCAL : 0))
+    , m_deviceBuffer(m_device,
+                     bufferUsage | CGPU_BUFFER_USAGE_FLAG_TRANSFER_DST,
+                     CGPU_MEMORY_PROPERTY_FLAG_DEVICE_LOCAL)
+  {
+
+  }
+
+  GgpuSyncBuffer::~GgpuSyncBuffer()
+  {
+    assert(m_size == 0);
+    assert(m_dirtyRangeBegin == 0);
+    assert(m_dirtyRangeEnd == 0);
+    assert(m_mappedHostMem == nullptr);
+    assert(m_hostBuffer.size() == 0);
+    assert(m_updateStrategy == UpdateStrategy::PersistentMapping || m_deviceBuffer.size() == 0);
+  }
+
+  uint8_t* GgpuSyncBuffer::getForReading(uint64_t byteOffset, uint64_t byteSize)
+  {
+    assert((byteOffset + byteSize) <= m_size);
+    return &m_mappedHostMem[byteOffset];
+  }
+
+  uint8_t* GgpuSyncBuffer::getForWriting(uint64_t byteOffset, uint64_t byteSize)
+  {
+    uint64_t rangeEnd = byteOffset + byteSize;
+    assert(rangeEnd <= m_size);
+    m_dirtyRangeBegin = std::min(byteOffset, m_dirtyRangeBegin);
+    m_dirtyRangeEnd = std::max(rangeEnd, m_dirtyRangeEnd);
+    return &m_mappedHostMem[byteOffset];
+  }
+
+  CgpuBuffer GgpuSyncBuffer::buffer() const
+  {
+    return m_updateStrategy == UpdateStrategy::PersistentMapping ?
+      m_hostBuffer.buffer() : m_deviceBuffer.buffer();
+  }
+
+  uint64_t GgpuSyncBuffer::byteSize() const
+  {
+    return m_size;
+  }
+
+  bool GgpuSyncBuffer::resize(CgpuDevice device,
+                              CgpuCommandBuffer commandBuffer,
+                              uint64_t newSize)
+  {
+    if (newSize == m_size)
+    {
+      assert(false); // Ideally resize() should only be called if its needed
+      return true;
+    }
+
+    // Unmap buffer before resize. New buffer is mapped later.
+    if (m_mappedHostMem)
+    {
+      cgpuUnmapBuffer(device, m_hostBuffer.buffer());
+      m_mappedHostMem = nullptr;
+    }
+
+    // Reset buffers if new size is 0.
+    if (newSize == 0)
+    {
+      m_hostBuffer.resize(0);
+      m_deviceBuffer.resize(0);
+      return true;
+    }
+
+    // Resize buffers.
+    if (m_updateStrategy == UpdateStrategy::OptimalStaging)
+    {
+      m_deviceBuffer.resize(/*m_device, commandBuffer,*/ newSize);
+    }
+
+    m_hostBuffer.resize(/*m_device, commandBuffer,*/ newSize);
+
+    if (!cgpuMapBuffer(device, m_hostBuffer.buffer(), (void**) &m_mappedHostMem))
+      return false;
+
+    m_size = newSize;
+
+    return true;
+  }
+
+  bool GgpuSyncBuffer::commitChanges(CgpuCommandBuffer commandBuffer)
+  {
+    if (m_dirtyRangeBegin > m_dirtyRangeEnd)
+    {
+      assert(false);
+      return false;
+    }
+
+    uint64_t commitSize = m_dirtyRangeEnd - m_dirtyRangeBegin;
+    if (!m_stager.stageToBuffer(&m_mappedHostMem[m_dirtyRangeBegin],
+                                commitSize,
+                                m_deviceBuffer.buffer(),
+                                m_dirtyRangeBegin))
+    {
+      return false;
+    }
+
+    m_dirtyRangeBegin = 0;
+    m_dirtyRangeEnd = 0;
+
+    return true;
+  }
+}
