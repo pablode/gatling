e0c115dc8e13e8130e61c7a89a791a7d1d402e3a
Pablo Delgado Krämer <private@pablode.com>
2024-02-06 11:45:54 +0100

    cmake: FindMDL now defines MDL_LIB_DIR

--- cmake/FindMDL.cmake
+++ cmake/FindMDL.cmake
@@ -5,32 +5,38 @@
 # MDL_FOUND
 # MDL_INCLUDE_DIR
 # MDL_SHARED_LIB
+# MDL_LIB_DIR
 #
 
 find_path(MDL_INCLUDE_DIR
   NAMES mi/mdl_sdk.h
   PATH_SUFFIXES include
-  HINTS ${MDL_ROOT})
+  HINTS ${MDL_ROOT}
+)
 
 if(WIN32)
-  set(MDL_BIN_DIR "nt-x86-64")
+  set(MDL_OS_LIB_DIR "nt-x86-64")
   set(CMAKE_FIND_LIBRARY_SUFFIXES ".dll")
 elseif(APPLE)
-  set(MDL_BIN_DIR "macosx-uni")
+  set(MDL_OS_LIB_DIR "macosx-uni")
 else()
-  set(MDL_BIN_DIR "linux-x86-64")
+  set(MDL_OS_LIB_DIR "linux-x86-64")
 endif()
 
 find_library(MDL_SHARED_LIB
   NAMES mdl_sdk libmdl_sdk
   HINTS ${MDL_ROOT}
-  PATH_SUFFIXES bin lib ${MDL_BIN_DIR}/lib
+  PATH_SUFFIXES bin lib ${MDL_OS_LIB_DIR}/lib
 )
+
+get_filename_component(MDL_LIB_DIR ${MDL_SHARED_LIB} DIRECTORY)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MDL
   DEFAULT_MSG
   MDL_INCLUDE_DIR
-  MDL_SHARED_LIB)
+  MDL_SHARED_LIB
+  MDL_LIB_DIR
+)
 
-mark_as_advanced(MDL_INCLUDE_DIR MDL_SHARED_LIB)
+mark_as_advanced(MDL_INCLUDE_DIR MDL_SHARED_LIB MDL_LIB_DIR)
