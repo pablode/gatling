c1af7f5406f7205a18a59670e1b2b311c2b53ea8
Pablo Delgado Krämer <private@pablode.com>
2024-02-19 22:59:53 +0100

    gi: explicitly vectorize ray offsetting code
    We mimic how it's done in NVIDIAGameWorks' NRD sample

--- src/gi/shaders/common.glsl
+++ src/gi/shaders/common.glsl
@@ -115,26 +115,14 @@ void orthonormal_basis(in vec3 n, out vec3 b1, out vec3 b2)
 vec3 offset_ray_origin(vec3 p, vec3 geom_normal)
 {
     const float origin = 1.0 / 32.0;
-    const float float_scale = 1.0 / 65536.0;
-    const float int_scale = 256.0;
+    const float floatScale = 1.0 / 65536.0;
+    const float intScale = 256.0;
 
-    ivec3 of_i = ivec3(
-        int_scale * geom_normal.x,
-        int_scale * geom_normal.y,
-        int_scale * geom_normal.z
-    );
+    ivec3 intOffset = ivec3(geom_normal * intScale);
+    vec3 intPos = intBitsToFloat(floatBitsToInt(p) + mix(-intOffset, intOffset, greaterThanEqual(p, vec3(0.0))));
+    vec3 floatOffset = geom_normal * floatScale;
 
-    vec3 p_i = vec3(
-        intBitsToFloat(floatBitsToInt(p.x) + ((p.x < 0.0) ? -of_i.x : of_i.x)),
-        intBitsToFloat(floatBitsToInt(p.y) + ((p.y < 0.0) ? -of_i.y : of_i.y)),
-        intBitsToFloat(floatBitsToInt(p.z) + ((p.z < 0.0) ? -of_i.z : of_i.z))
-    );
-
-    return vec3(
-        abs(p.x) < origin ? (p.x + float_scale * geom_normal.x) : p_i.x,
-        abs(p.y) < origin ? (p.y + float_scale * geom_normal.y) : p_i.y,
-        abs(p.z) < origin ? (p.z + float_scale * geom_normal.z) : p_i.z
-    );
+    return mix(p + floatOffset, intPos, greaterThanEqual(abs(p), vec3(origin)));
 }
 #else
 vec3 offset_ray_origin(vec3 p, vec3 geom_normal)
