1802175201e2d50aba25a48cf7215a7878c2f699
Pablo Delgado Krämer <private@pablode.com>
2024-05-24 15:15:40 +0200

    gi: introduce medium index to shade ray payload bitfield

--- src/gi/shaders/rp_main_payload.glsl
+++ src/gi/shaders/rp_main_payload.glsl
@@ -1,9 +1,23 @@
 #include "common.glsl"
 
+#define SHADE_RAY_PAYLOAD_MEDIUM_IDX_MASK 0x0f000000u
+#define SHADE_RAY_PAYLOAD_MEDIUM_IDX_OFFSET 24
+#define SHADE_RAY_PAYLOAD_WALK_MASK 0x00fff000u
+#define SHADE_RAY_PAYLOAD_WALK_OFFSET 12
+#define SHADE_RAY_PAYLOAD_BOUNCES_MASK 0x00000fffu
+#define SHADE_RAY_PAYLOAD_TERMINATE_FLAG 0x80000000u
+
 struct ShadeRayPayload
 {
     /* inout */ vec3 throughput;
-    /* inout */ uint bitfield; // MSB: inside/outside. rest: bounce count
+
+    /*               1000 0000 0000 0000 0000 0000 0000 0000 terminate
+     *               0111 0000 0000 0000 0000 0000 0000 0000 unused
+     *               0000 1111 0000 0000 0000 0000 0000 0000 medium index [0, 256)
+     *               0000 0000 1111 1111 1111 0000 0000 0000 walk length [0, 4096)
+     *               0000 0000 0000 0000 0000 1111 1111 1111 bounces [0, 4096) */
+    /* inout */ uint bitfield;
+
     /* inout */ vec3 radiance;
     /* inout */ RNG_STATE_TYPE rng_state;
     /* out */   vec3 ray_origin;
