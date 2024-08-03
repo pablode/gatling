af4ce6c63a31e1fd94cdb2551c3c48d55a71a829
Pablo Delgado Krämer <private@pablode.com>
2024-08-03 18:57:18 +0200

    ggpu: fix encoding of some files not being UTF-8

--- src/ggpu/gtl/ggpu/DenseDataStore.h
+++ src/ggpu/gtl/ggpu/DenseDataStore.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2023 Pablo Delgado Kr‰mer
+// Copyright (C) 2023 Pablo Delgado Kr√§mer
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
