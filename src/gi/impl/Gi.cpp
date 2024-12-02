322b99554538254c71d11ba992e2fec255a79abf
Pablo Delgado Krämer <private@pablode.com>
2024-12-02 20:36:01 +0100

    gi: remap vertices with meshoptimizer

--- src/gi/impl/Gi.cpp
+++ src/gi/impl/Gi.cpp
@@ -57,6 +57,8 @@
 
 #include <MaterialXCore/Document.h>
 
+#include <meshoptimizer.h>
+
 namespace mx = MaterialX;
 
 constexpr static const float BYTES_TO_MIB = 1.0f / (1024.0f * 1024.0f);
@@ -263,6 +265,33 @@ namespace gtl
     glm::vec2 e = _EncodeOctahedral(v);
     e = e * 0.5f + 0.5f;
     return glm::packUnorm2x16(e);
+  }
+
+  uint32_t _PrimvarTypeSize(GiPrimvarType type)
+  {
+    switch (type)
+    {
+    case GiPrimvarType::Float:
+    case GiPrimvarType::Int:
+      return 4;
+
+    case GiPrimvarType::Int2:
+    case GiPrimvarType::Vec2:
+      return 2 * 4;
+
+    case GiPrimvarType::Int3:
+    case GiPrimvarType::Vec3:
+      return 3 * 4;
+
+    case GiPrimvarType::Int4:
+    case GiPrimvarType::Vec4:
+      return 4 * 4;
+
+    default:
+      assert(false);
+      GB_ERROR("coding error: unhandled type size!");
+      return 0;
+    }
   }
 
   bool _giResizeRenderBufferIfNeeded(GiRenderBuffer* renderBuffer, uint32_t pixelStride)
@@ -535,20 +564,70 @@ fail:
     return offset;
   }
 
-  GiMesh* giCreateMesh(GiScene* scene, const GiMeshDesc& desc)
+  GiMeshCpuData _ProcessMeshData(uint32_t vertexCount, const GiVertex* vertices,
+                                 uint32_t faceCount, const GiFace* faces,
+                                 const std::vector<GiPrimvarData>& primvars)
   {
+    std::vector<meshopt_Stream> streams;
+    streams.reserve(primvars.size());
+
+    streams.push_back(meshopt_Stream{ (void*) &vertices[0], sizeof(GiVertex), sizeof(GiVertex) });
+    for (const GiPrimvarData& p : primvars)
+    {
+      if (p.interpolation == GiPrimvarInterpolation::Constant)
+      {
+        continue;
+      }
+      uint32_t sizeAndStride = _PrimvarTypeSize(p.type);
+      streams.push_back(meshopt_Stream{ (void*) &p.data[0], sizeAndStride, sizeAndStride });
+    }
+
+    std::vector<uint32_t> remap(vertexCount);
+    uint32_t newVertexCount = meshopt_generateVertexRemapMulti(remap.data(), &faces[0].v_i[0], faceCount * 3,
+                                                               vertexCount, streams.data(), streams.size());
+
+    std::vector<GiVertex> newVertices(newVertexCount);
+    meshopt_remapVertexBuffer((void*) newVertices.data(), (void*) &vertices[0],
+                              vertexCount, sizeof(GiVertex), remap.data());
+
+    std::vector<GiFace> newFaces(faceCount);
+    meshopt_remapIndexBuffer((uint32_t*) &newFaces[0], &faces[0].v_i[0], faceCount * 3, remap.data());
+
+    std::vector newPrimvars = primvars;
+    for (uint32_t i = 0; i < primvars.size(); i++)
+    {
+      const auto& o = primvars[i];
+
+      if (o.interpolation == GiPrimvarInterpolation::Constant)
+      {
+        continue;
+      }
+
+      auto& n = newPrimvars[i];
+      uint32_t typeSize = _PrimvarTypeSize(o.type);
+
+      n.data.resize(typeSize * vertexCount);
+      meshopt_remapVertexBuffer(&n.data[0], &o.data[0], vertexCount, typeSize, remap.data());
+    }
+
     GiMeshCpuData cpuData = {
-      .faces = std::vector<GiFace>(&desc.faces[0], &desc.faces[desc.faceCount]),
-      .primvars = desc.primvars,
-      .vertices = std::vector<GiVertex>(&desc.vertices[0], &desc.vertices[desc.vertexCount])
+      .faces = newFaces,
+      .primvars = newPrimvars,
+      .vertices = newVertices
     };
 
+    return cpuData;
+  }
+
+  GiMesh* giCreateMesh(GiScene* scene, const GiMeshDesc& desc)
+  {
     GiMesh* mesh = new GiMesh {
       .transform = glm::mat3x4(1.0f),
       .flipFacing = desc.isLeftHanded,
       .id = desc.id,
       .scene = scene,
-      .cpuData = cpuData,
+      .cpuData = _ProcessMeshData(desc.vertexCount, desc.vertices,
+                                  desc.faceCount, desc.faces, desc.primvars),
       .name = desc.name
     };
 
